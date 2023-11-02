29348e9407d5e0d9b19cc9ebf5b5db031bd6e153
Tillymolete <Mahlatsemolete@gmail.com>
2023-11-02 11:18:10 -0700

    perfectbinary

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -8,9 +8,9 @@
 
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-	if (node == NULL || node-.parent == NULL)
+	if (node == NULL || node->parent == NULL)
 		return (NULL);
 	if (node->parent->right == node)
 		return (node->parent->left);
-	return (node->parent-right);
+	return (node->parent->right);
 }
